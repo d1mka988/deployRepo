379703a04d855314a5f362db64f0b171d509bbc3
d1mka988 <dimayarema@gmail.com>
2018-10-23 19:37:54 +0300

    Wellcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Mathefucks
                 </div>
 
                 <div class="links">
