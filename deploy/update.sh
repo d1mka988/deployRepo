37ea9272adf0d2f2d0b26ccd810324854fef2776
d1mka988 <dimayarema@gmail.com>
2018-10-23 18:06:52 +0300

    Update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo -R chown -R www-data: cd /var/www/deploy-$DEPLOYMENT_ID
+
+sudo chown -R www-data: cd /var/www/deploy-latest
+sudo mv /var/www/deploy-latest /var/www/deploy-$DEPLOYMENT_ID
 cd /var/www/deploy-$DEPLOYMENT_ID
 sudo -H -u www-data composer install
 sudo -H -u www-data php artisan key:generate
