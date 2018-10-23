718515245859d61c6f21eeb2af993d45f4f34126
d1mka988 <root@ip-172-31-26-109.eu-west-1.compute.internal>
2018-10-23 17:42:48 +0300

    Vars in spec

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo -R chown -R www-data: cd /var/www/html/
-cd /var/www/html/
+sudo -R chown -R www-data: cd /var/www/deploy-$DEPLOYMENT_ID
+cd /var/www/deploy-$DEPLOYMENT_ID
 sudo -H -u www-data composer install
 sudo -H -u www-data php artisan key:generate
 sudo -H -u www-data php artisan migrate
+sudo -H -u www-data ln -sfn /var/www/deploy-$DEPLOYMENT_ID /var/www/html
+
 
