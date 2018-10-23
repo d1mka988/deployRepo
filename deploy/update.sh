02ee1cb3085164a0a16dc8cff8bd444b5010d059
d1mka988 <dimayarema@gmail.com>
2018-10-23 19:39:08 +0300

    Update Spec

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo cp /var/www/html.bak/.env  /var/www/deploy-latest/.env
-sudo chown -R www-data:  /var/www/deploy-latest
-sudo mv /var/www/deploy-latest /var/www/deploy-$DEPLOYMENT_ID
-cd /var/www/deploy-$DEPLOYMENT_ID
-sudo -H -u www-data composer install
+#sudo cp /var/www/html.bak/.env  /var/www/deploy-latest/.env
+sudo chown -R www-data:  /var/www/html/
+#sudo mv /var/www/deploy-latest /var/www/deploy-$DEPLOYMENT_ID
+cd /var/www/html
+sudo -H -u www-data composer update
 sudo -H -u www-data php artisan key:generate
 sudo -H -u www-data php artisan migrate
-sudo -H -u www-data ln -sfn /var/www/deploy-$DEPLOYMENT_ID /var/www/html
+#sudo -H -u www-data ln -sfn /var/www/deploy-$DEPLOYMENT_ID /var/www/html
 
 
