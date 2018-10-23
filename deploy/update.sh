cce989f1a6c0090bf308c86855c96572c56163d6
root <root@ip-172-31-26-109.eu-west-1.compute.internal>
2018-10-23 15:25:44 +0300

    Artisan added

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo -R chown -R www-data: cd /var/www/html/
 cd /var/www/html/
-sudo -u www-data composer install
-sudo -u www-data php artisan migrate
+sudo -H -u www-data composer install
+sudo -H -u www-data php artisan key:generate
+sudo -H -u www-data php artisan migrate
+
