c62d8b08474906169ebee625d7b9f6495292076e
d1mka988 <dimayarema@gmail.com>
2018-10-24 15:20:09 +0300

    update script2

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-#sudo cp /var/www/html.bak/.env  /var/www/deploy-latest/.env
+sudo cp /var/www/html.bak/.env  /var/www/deploy-latest/.env
 sudo chown -R www-data:  /var/www/html/
-#sudo mv /var/www/deploy-latest /var/www/deploy-$DEPLOYMENT_ID
-cd /var/www/html
+sudo mv /var/www/deploy-latest /var/www/deploy-$DEPLOYMENT_ID
+cd /var/www/deploy-$DEPLOYMENT_ID
 sudo -H -u www-data composer update
-sudo -H -u www-data php artisan key:generate
+#sudo -H -u www-data php artisan key:generate
+# Clear any previous cached views
 sudo -H -u www-data php artisan migrate
-#sudo -H -u www-data ln -sfn /var/www/deploy-$DEPLOYMENT_ID /var/www/html
+# Clear any previous cached views
+sudo -H -u www-data php artisan config:clear
+sudo -H -u www-data php artisan cache:clear
+sudo -H -u www-data php artisan view:clear
+
+# Opimize the application
+sudo -H -u www-data php artisan config:cache
+sudo -H -u www-data php artisan optimize
+sudo -H -u www-data ln -sfn /var/www/deploy-$DEPLOYMENT_ID /var/www/html
 
 
